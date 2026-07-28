fe45cf145dfc75731a41ac13676831879a6793eb
annmolpatalay <anmolpatalay@gmail.com>
2026-07-28 22:24:08 +0530

    changes for adding authentication

--- Routers/Auth.py
+++ Routers/Auth.py
@@ -3,41 +3,57 @@ from database import get_db
 from typing import Annotated
 from sqlalchemy.orm import Session
 from models import Users
-from pydantic import BaseModel
-from datetime import datetime
-from utils import hash_password
+from pydantic import BaseModel,EmailStr
+from datetime import datetime,timedelta,timezone
+from utils import hash_password,bcrypt_context
+from jose import jwt
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
 router = APIRouter(
     prefix="/Auth",
     tags=['Auth']
 )
 
 class EnterUserData(BaseModel):
-    id : int
-    email : str
-    hashed_password : str
-    created_at : datetime
+    email : EmailStr
+    password : str
 
     model_config = {
         "json_schema_extra":{
             "example": {
-                "id": 0,
                 "email": "string",
-                "hashed_password": "string",
-                "created_at" : datetime.now()
+                "password": "string",
             }
         }
     }
+#################################### Access_Token ####################################
+SECRET_KEY = "DHIWEHF83YFWHFKJEBF3E0RU4390RUEDFNWEBFWEFlkheugerhfiuf98rfbfb8U0ihbhjbHVUUG88UBJVGCYTDFU879E7983289H34JBWJEOGUE89GEEVBEJVHEIYH"
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+def create_token(email:str,user_id:int):
+    expire = datetime.now(timezone.utc) + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
 
+    payload = {
+        "sub" : email,
+        "id" : user_id,
+        "exp" : expire
+    }
+    return jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/Auth/auth/login")
+auth_dependency = Depends(oauth2_scheme)
+#################################### END POINTS ####################################
 db_dependency = Annotated[Session,Depends(get_db)]
 
 @router.post("/auth/registering new user")
 async def register_new_user(db : db_dependency,user: EnterUserData):
     register_new_user_model = Users(
-        id = user.id,
         email = user.email,
-        hashed_password = hash_password(user.hashed_password),
+        hashed_password = hash_password(user.password),
         created_at = datetime.now()
     )
     db.add(register_new_user_model)
@@ -47,11 +63,25 @@ async def register_new_user(db : db_dependency,user: EnterUserData):
     return {'ok':'added'}
 
 @router.post("/auth/login")
-async def login(): pass
+async def login(db: db_dependency,form_data : OAuth2PasswordRequestForm = Depends()): 
+    email_id = form_data.username
+    password1 = form_data.password
+    print(email_id)
+    print(password1)
+    user_detail = db.query(Users).filter(Users.email == email_id).first()
+    if user_detail is None:
+        return {"user":"username not available"}
+    if bcrypt_context.verify(password1,user_detail.hashed_password):
+        token = create_token(email = user_detail.email,user_id=user_detail.id)
+        return {"access_token": token, "token_type":'bearer'}
+    else:
+        return{"failed","not authenticated"}
 
-@router.get("/auth/me/{id}")
-async def get_current_user(db : db_dependency,id: int = Path(ge=0)): 
-    user_data = db.query(Users).filter(Users.id == id).first()
-    return user_data
+
+@router.get("/auth/me/")
+async def get_current_user(db : db_dependency,user: str = auth_dependency): 
+    payload = jwt.decode(user,SECRET_KEY,algorithms=[ALGORITHM])
+    user_detail = db.query(Users).filter(Users.id == payload["id"]).first()
+    return user_detail.created_at,user_detail.email
 
     
